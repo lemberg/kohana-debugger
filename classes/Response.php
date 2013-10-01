7033c4a5ab8e085ee07c950b53661abaf45acaae
Serhiy Dmytriv <serhij.dmytriv@lemberg.co.uk>
2013-10-01 15:57:07 +0300

    added comment to explain why the Response was changed

--- classes/Response.php
+++ classes/Response.php
@@ -1,5 +1,9 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
+/*
+ * This code allows to send custom x-kohana-debugger header
+ * with all the debug info at the and of the request.
+ */
 class Response extends Kohana_Response
 {
 	public function send_headers($replace = FALSE, $callback = NULL)
