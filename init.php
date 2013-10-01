7be4e72e0a2a5ac7cdf44cdd67a69b4663d3cf87
Serhiy Dmytriv <serhij.dmytriv@lemberg.co.uk>
2013-10-01 15:57:17 +0300

    removed unused code

--- init.php
+++ init.php
@@ -1,10 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-if (Kohana::$config->load('debugger.auto_render') === TRUE)
-{
-	//register_shutdown_function(array('Debugger', 'render'));
-}
-
 if (Kohana::$config->load('debugger.panels.logs') === TRUE)
 {
 	Kohana::$log->attach(Log_Debugger::instance());
